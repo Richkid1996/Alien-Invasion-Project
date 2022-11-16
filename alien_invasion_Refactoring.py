9f119e48a98c3416343bf22fe556a43c96959003
Rico Robinson <106034410+Richkid1996@users.noreply.github.com>
2022-11-16 17:25:56 -0600

    Update alien_invasion_Refactoring.py
    On this update, we just Refactored the _check_events()

--- alien_invasion_Refactoring.py
+++ alien_invasion_Refactoring.py
@@ -29,23 +29,28 @@ class AlienInvasion:
 	def _check_events(self):
 		"""Respond to keypresses and mouse events."""
 		for event in pygame.event.get():
-				if event.type == pygame.QUIT:
-					sys.exit()
-				elif event.type == pygame.KEYDOWN:
-					if event.type == pygame.K_RIGHT:
-						self.ship.moving_right = True
-					elif event.key == pygame.K_LEFT:
-						self.ship.moving_left = True
-						#Move the ship to the right.
-						self.ship.rect.x += 1
-
-				elif event.type == pygame.KEYUP:
-					if event.key == pygame.K_RIGHT:
-						self.ship.moving_right = False
-					elif event.key == pygame.K_LEFT:
-						self.ship.moving_left = False
-						# Move the ship to the right.
-						self.ship.rect.x += 1
+			if event.type == pygame.QUIT:
+				sys.exit()
+			elif event.type == pygame.KEYDOWN:
+				self._check_keydown_events(event)
+			elif event.type == pygame.KEYUP:
+				self._check_keyup_events(event)
+					
+					
+	def _check_keydown_events(self, event):
+		"""Respond to key release."""
+            if event.type == pygame.K_RIGHT:
+		self.ship.moving_right = True
+	    elif event.key == pygame.K_LEFT:
+		self.ship.moving_left = True
+						
+        def _check_keyup_events(self, event):
+	    """Respond to key releases."""
+	    if event.key == pygame.K_RIGHT:
+		self.ship.moving_right = False
+	    elif event.key == pygame.K_LEFT:
+		self.ship.moving_left = False
+						
 
 	def _update_screen(self):
 		"""Update images on the screen, and flip to the new screen"""
