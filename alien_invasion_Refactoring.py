8bafb2fce7b45d8cc5c1f350972460d678842134
Rico Robinson <106034410+Richkid1996@users.noreply.github.com>
2022-11-22 19:48:38 -0600

    Update alien_invasion_Refactoring.py

--- alien_invasion_Refactoring.py
+++ alien_invasion_Refactoring.py
@@ -26,8 +26,19 @@ class AlienInvasion:
 		while True:
 			self._check_events()
 			self.ship.update()
-			self.bullets.update()
+			self._update_bullets()
 			self._update_screen()
+			
+	def _update_bullets(self):
+		"""Update position of bullets and get rid of old bullets."""
+		#Update bullet postions.
+		self.bullets.update()
+
+		     #Get rid of bullets that have disappeared.
+		for bullet in self.bullets.copy():
+			if bullet.rect.bottom <= 0:
+					self.bullets.remove(bullet)
+ 
 
 
 			# Redraw the screen during each pass through the loop.
