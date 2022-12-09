67c69a4e0a5f4bef0ec0a901d9349581828977d9
Rico Robinson <106034410+Richkid1996@users.noreply.github.com>
2022-12-08 19:01:24 -0600

    Update settings.py

--- settings.py
+++ settings.py
@@ -12,6 +12,13 @@ class Settings:
 		self.ship_speed = 1.5
 		self.ship_limit = 3
 		
+		
+		#Alien settings
+		self.alien_speed = 1.0
+		self.fleet_drop_speed = 1
+		#fleet_direction of 1 represents right; -1 represents left.
+		self.fleet_direction = 1
+		
 		#Bullet settings
 		self.bullet_speed = 1.0
 		self.bullet_width = 3
