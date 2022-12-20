21876f901829967e947e2b9c2fa85a6d7fa8f2ba
Rico Robinson <106034410+Richkid1996@users.noreply.github.com>
2022-12-19 19:24:32 -0600

    Update settings.py

--- settings.py
+++ settings.py
@@ -2,7 +2,7 @@ class Settings:
 	"""A class to store all settings for Alein Invasion."""
 
 	def __init__(self):
-		"""Initialize the game's settings."""
+		"""Initialize the game's static settings."""
 		#Screen settings
 		self.screen_width = 1200
 		self.screen_height = 800
@@ -25,3 +25,8 @@ class Settings:
 		self.bullet_height = 15
 		self.bullet_color = (255,0,0)
 		self.bullets_allowed = 3
+		
+		#How quickly the game speeds up
+		self.speedup_scale = 1.1
+		
+		self.initialize_dynamic_settings()
